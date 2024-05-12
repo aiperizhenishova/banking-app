8c8320b71146e81d710b733d3f45d07c413e397d
Aiperi <zhenishovaaiperi@gmail.com>
2024-05-12 20:26:03 +0600

    change

--- src/main/java/com/example/bankingapp/repositories/UserRepository.java
+++ src/main/java/com/example/bankingapp/repositories/UserRepository.java
@@ -2,9 +2,18 @@ package com.example.bankingapp.repositories;
 
 import com.example.bankingapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    String getUsername();
+
+    String getPassword();
+
+    Collection<? extends GrantedAuthority> getRoles();
 }
