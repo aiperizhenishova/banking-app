d074f8d4b437712ae1fff801ce20ac9c23e38cf0
Aiperi <zhenishovaaiperi@gmail.com>
2024-05-15 12:39:07 +0600

    change

--- src/main/java/com/example/bankingapp/repositories/UserRepository.java
+++ src/main/java/com/example/bankingapp/repositories/UserRepository.java
@@ -1,19 +1,11 @@
 package com.example.bankingapp.repositories;
 
-import com.example.bankingapp.entities.User;
+import com.pokemonreview.api.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-    String getUsername();
-
-    String getPassword();
-
-    Collection<? extends GrantedAuthority> getRoles();
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
 }
--- src/main/java/com/example/bankingapp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankingapp.service;
-
-import com.example.bankingapp.entities.User;
-
-public interface UserService {
-    User findByUsername(String username);
-    User save(User user);
-    // Другие методы, например, для аутентификации, обновления пароля и т. д.
-}
